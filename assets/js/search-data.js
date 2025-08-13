1a267f9e9ab1942582a6e44335f4abb8e7bc5153
SampsonML <matt.sampson@princeton.edu>
2025-08-13 14:48:21 +0000

    Deploying to gh-pages from @ SampsonML/SampsonML.github.io@437aeb4656bee4a9193b83d0fe420d37b420469b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -443,13 +443,6 @@ ninja.data = [{
           window.open("https://www.linkedin.com/in/matt-sampson-b56b8113b/", "_blank");
         },
       },{
-        id: 'social-orcid',
-        title: 'ORCID',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://orcid.org/0000-0001-5748-5393", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
