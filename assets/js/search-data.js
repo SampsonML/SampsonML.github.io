1168e07e13c47b67cc951a963bd6c50a6b5a05cf
SampsonML <matt.sampson@princeton.edu>
2025-09-29 23:54:38 +0000

    Deploying to gh-pages from @ SampsonML/SampsonML.github.io@ba45c2f5b3069516e668c675d5be4a18f1420eae 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -37,7 +37,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "books-the-godfather",
+        },{id: "post-dynamics-of-learning",
+        
+          title: "Dynamics of learning",
+        
+        description: "Creating a generative learning rate scheduler with latent ODEs",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/lode-scheduler/";
+          
+        },
+      },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
