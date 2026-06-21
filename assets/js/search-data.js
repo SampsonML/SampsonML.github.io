9085b8141c18b8c6a48cadae3757a74d4429e1e2
SampsonML <matt.sampson@princeton.edu>
2026-06-21 20:59:25 +0000

    Deploying to gh-pages from @ SampsonML/SampsonML.github.io@d429e9a996d155f5f983413868f514a3a48a14ad 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
+  },{id: "nav-research-themes",
+          title: "research themes",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/research-themes/";
+          },
+        },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
